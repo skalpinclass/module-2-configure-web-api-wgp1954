3e71ea7bd332c06097ec93ea54893d6a560f61bd
Stephen Kalpin <skalpin@gmail.com>
2019-04-05 10:37:28 -0500

    initial project commit

---
+++ Blog.WebApi/Controllers/BlogController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Blog.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class BlogController : ControllerBase
+    {
+        /*
+        [HttpGet]
+        [Produces("application/json")]
+        [ProducesResponseType(200)]
+        public ActionResult<IEnumerable<Blog>> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [Produces("application/json")]
+        [ProducesResponseType(200)]
+        public void Post([FromBody] Blog value)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id}")]
+        [Produces("application/json")]
+        [ProducesResponseType(200)]
+        public void Put(int id, [FromBody] Blog value)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(200)]
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+        */
+    }
+}
